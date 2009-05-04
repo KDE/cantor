fbb81d29fbb3da835f5ff5a4154c573764ef093f
Laurent Montel <montel@kde.org>
2009-05-04 07:01:31 +0000

    extract all ui file
    svn path=/trunk/playground/edu/mathematik/; revision=963199

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc` >> rc.cpp
+$EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 12
+$EXTRACTRC `find . -name \*.kcfg` >> rc.cpp || exit 13
 $XGETTEXT *.cpp -o $podir/mathematik.pot
 rm -f *.cpp
