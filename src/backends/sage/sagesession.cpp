30450b65125ce328604ccfb463b5d6f05b1fa803
Alexander Rieder <alexanderrieder@gmail.com>
2009-05-27 20:09:18 +0000

    don't show the "sage crashed" message, if it the process was closed because logout() was called
    svn path=/trunk/playground/edu/mathematik/; revision=973783

--- src/backends/sage/sagesession.cpp
+++ src/backends/sage/sagesession.cpp
@@ -77,6 +77,7 @@ void SageSession::logout()
 {
     kDebug()<<"logout";
     interrupt();
+    disconnect(m_process, SIGNAL(finished ( int,  QProcess::ExitStatus )), this, SLOT(processFinished(int, QProcess::ExitStatus)));
     m_process->pty()->write("exit\n");
 
     m_process->deleteLater();
