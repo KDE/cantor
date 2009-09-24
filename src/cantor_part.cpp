e320faf3b6e4f23540143b95adabfd63ca155199
Alexander Rieder <alexanderrieder@gmail.com>
2009-09-24 19:53:50 +0000

    use the same i18n catalog for cantor and cantorpart
    svn path=/trunk/kdereview/cantor/; revision=1027804

--- src/cantor_part.cpp
+++ src/cantor_part.cpp
@@ -201,7 +201,7 @@ KAboutData *CantorPart::createAboutData()
     // the non-i18n name here must be the same as the directory in
     // which the part's rc file is installed ('partrcdir' in the
     // Makefile)
-    KAboutData *aboutData = new KAboutData("cantorpart", 0, ki18n("CantorPart"), "0.1");
+    KAboutData *aboutData = new KAboutData("cantorpart",  "cantor",  ki18n("CantorPart"), "0.1");
     aboutData->addAuthor(ki18n("Alexander Rieder"), KLocalizedString(), "alexanderrieder@gmail.com");
     return aboutData;
 }
