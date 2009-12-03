a973ea3d0f87b161fdcdd9f08b57e9653566b825
Milian Wolff <mail@milianw.de>
2009-12-03 19:02:34 +0000

    fix indendation
    svn path=/trunk/KDE/kdeedu/cantor/; revision=1057983

--- src/lib/backend.h
+++ src/lib/backend.h
@@ -59,14 +59,14 @@ class CANTOR_EXPORT Backend : public QObject, public KXMLGUIClient
     enum Capability{
         Nothing = 0x0,             ///< the Backend doesn't support any of the optional features
         LaTexOutput = 0x1,         ///< it can output results as LaTeX code
-	InteractiveMode = 0x2,     /**< it supports an interactive workflow. (It means a command 
-				        can ask for additional input while running
-				   */
-	SyntaxHighlighting = 0x4,  ///< it offers a custom Syntax Highlighter 
-	TabCompletion = 0x8,       ///< it offers completion of partially typed commands
-	SyntaxHelp = 0x10          /**< it offers help about a commands syntax, that will
-				        be shown in a tooltip
-				   */
+        InteractiveMode = 0x2,     /**< it supports an interactive workflow. (It means a command
+                                        can ask for additional input while running
+                                    */
+        SyntaxHighlighting = 0x4,  ///< it offers a custom Syntax Highlighter
+        TabCompletion = 0x8,       ///< it offers completion of partially typed commands
+        SyntaxHelp = 0x10          /**< it offers help about a commands syntax, that will
+                                        be shown in a tooltip
+                                    */
     };
     Q_DECLARE_FLAGS(Capabilities, Capability)
 
