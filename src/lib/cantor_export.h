1a34103b939f7bc5083b4afb01b767c4eedf7b6e
Patrick Spendrin <ps_ml@gmx.de>
2011-08-30 15:47:22 +0200

    fix exports on windows & make link

--- src/lib/cantor_export.h
+++ src/lib/cantor_export.h
@@ -34,6 +34,16 @@
 # endif
 #endif
 
+#ifndef CANTORTEST_EXPORT
+# if defined(MAKE_CANTORTEST_LIB)
+// We are building this library
+#  define CANTORTEST_EXPORT KDE_EXPORT
+# else
+// We are using this library
+#  define CANTORTEST_EXPORT KDE_IMPORT
+# endif
+#endif
+
 # ifndef CANTOR_EXPORT_DEPRECATED
 #  define CANTOR_EXPORT_DEPRECATED KDE_DEPRECATED CANTOR_EXPORT
 # endif
