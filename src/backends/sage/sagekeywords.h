6e7eb3189ca1564d52a373f7ea3dfed96f1426e3
Martin Küttler <martin.kuettler@gmx.de>
2012-03-17 10:49:18 +0100

    Changed the licence text in sagekeywords.h
    The old licence text in sagekeywords.h was not recognised for some
    reason, so I'm trying one from
    http://techbase.kde.org/Policies/Licensing_Policy

--- src/backends/sage/sagekeywords.h
+++ src/backends/sage/sagekeywords.h
@@ -1,8 +1,13 @@
 /*
+    Copyright 2012  Martin Kuettler <martin.kuettler@gmail.com>
+
     This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy 
+    defined in Section 14 of version 3 of the license.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -10,19 +15,15 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA  02110-1301, USA.
-
-    ---
-    Copyright (C) 2012 Martin Kuettler <martin.kuettler@gmail.com>
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef _SAGEKEYWORDS_H
 #define _SAGEKEYWORDS_H
 
 #include <QStringList>
 
-/*
+/**
   Class to store all Sage keywords (i.e. Python keywords)
   It is similiar to MaximaKeywords or ScilabKeywords, but for Sage we only
   need to store actual keywords, as variables and functions can be fetched from
