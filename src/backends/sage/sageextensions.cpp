6a36ef3c68da11226a56225e16f807f39f2551aa
Yuri Chornoivan <yurchor@ukr.net>
2015-06-07 18:04:05 +0300

    Fix typo

--- src/backends/sage/sageextensions.cpp
+++ src/backends/sage/sageextensions.cpp
@@ -44,7 +44,7 @@ QString SageScriptExtension::runExternalScript(const QString& path)
 
 QString SageScriptExtension::scriptFileFilter()
 {
-    return i18n("Python script file (*.py);;Sage script file( *.sage)");
+    return i18n("Python script file (*.py);;Sage script file (*.sage)");
 }
 
 QString SageScriptExtension::highlightingMode()
