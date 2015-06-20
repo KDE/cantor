KDE/cantor

commit 8711f0596f03658de1627a2862c1893cb766e8bc

Port KTabWidget to QTabWidget REVIEW: 123725

src/cantor.cpp
@@ -32,7 +32,7 @@
 #include <KStatusBar>
 #include <QIcon>
 #include <KLocale>
-#include <KTabWidget>
+#include <QTabWidget>
 #include <KConfigDialog>
 #include <KTextEdit>
 #include <QTextBrowser>
@@ -70,7 +70,7 @@ CantorShell::CantorShell()
 
     createGUI(0);
 
-    m_tabWidget=new KTabWidget(this);
+    m_tabWidget=new QTabWidget(this);
     m_tabWidget->setTabsClosable(true);
     m_tabWidget->setMovable(true);
     m_tabWidget->setDocumentMode(true);

src/cantor.h
@@ -27,7 +27,7 @@
 
 #include <KParts/ReadWritePart>
 
-class KTabWidget;
+class QTabWidget;
 class KTextEdit;
 
 /**
@@ -98,7 +98,7 @@ private:
 private:
     QList<KParts::ReadWritePart *> m_parts;
     KParts::ReadWritePart* m_part;
-    KTabWidget* m_tabWidget;
+    QTabWidget* m_tabWidget;
     QList<QDockWidget*> m_panels;
     QDockWidget* m_helpDocker;
 };

src/assistants/advancedplot/advancedplotdialog.ui
@@ -47,18 +47,10 @@
     </widget>
    </item>
    <item>
-    <widget class="KTabWidget" name="directivesTabs"/>
+    <widget class="QTabWidget" name="directivesTabs"/>
    </item>
   </layout>
  </widget>
- <customwidgets>
-  <customwidget>
-   <class>KTabWidget</class>
-   <extends>QTabWidget</extends>
-   <header>ktabwidget.h</header>
-   <container>1</container>
-  </customwidget>
- </customwidgets>
  <resources/>
  <connections/>
 </ui>

src/backends/qalculate/plotassistant/qalculateplotdialog.ui
@@ -15,7 +15,7 @@
   </property>
   <layout class="QGridLayout" name="gridLayout">
    <item row="0" column="0">
-    <widget class="KTabWidget" name="ktabwidget">
+    <widget class="QTabWidget" name="qtabwidget">
      <property name="currentIndex">
       <number>0</number>
      </property>
@@ -625,15 +625,8 @@
    <extends>QComboBox</extends>
    <header>kcombobox.h</header>
   </customwidget>
-  <customwidget>
-   <class>KTabWidget</class>
-   <extends>QTabWidget</extends>
-   <header>ktabwidget.h</header>
-   <container>1</container>
-  </customwidget>
  </customwidgets>
  <tabstops>
-  <tabstop>ktabwidget</tabstop>
   <tabstop>functionTable</tabstop>
   <tabstop>addButton</tabstop>
   <tabstop>removeButton</tabstop>

src/backends/qalculate/settings.ui
@@ -12,7 +12,7 @@
   </property>
   <layout class="QVBoxLayout" name="verticalLayout">
    <item>
-    <widget class="KTabWidget" name="ktabwidget">
+    <widget class="QTabWidget" name="qtabwidget">
      <property name="currentIndex">
       <number>0</number>
      </property>
@@ -627,19 +627,12 @@
    <header>kcombobox.h</header>
   </customwidget>
   <customwidget>
-   <class>KTabWidget</class>
-   <extends>QTabWidget</extends>
-   <header>ktabwidget.h</header>
-   <container>1</container>
-  </customwidget>
-  <customwidget>
    <class>KIntNumInput</class>
    <extends>QWidget</extends>
    <header>knuminput.h</header>
   </customwidget>
  </customwidgets>
  <tabstops>
-  <tabstop>ktabwidget</tabstop>
   <tabstop>kcfg_base</tabstop>
   <tabstop>kcfg_fractionFormat</tabstop>
   <tabstop>kcfg_minExp</tabstop>

commit eb9e238fe676ce2337981fb1de4d79cc2f3ba034

Fix "close" button in tabbar after porting from KTabWidget to QTabWidget
Use signal QTabWidget::tabCloseRequested(int) instead of
KTabWidget::closeRequest(QWidget*).

Problem introduced by commit 8711f0596f03658de1627a2862c1893cb766e8bc
"Port KTabWidget to QTabWidget", review request 123966.

REVIEW: 124040

src/cantor.cpp
@@ -78,7 +78,7 @@ CantorShell::CantorShell()
     setCentralWidget(m_tabWidget);
 
     connect(m_tabWidget, SIGNAL(currentChanged(int)), this, SLOT(activateWorksheet(int)));
-    connect(m_tabWidget, SIGNAL(closeRequest (QWidget *)), this, SLOT(closeTab(QWidget*)));
+    connect(m_tabWidget, SIGNAL(tabCloseRequested(int)), this, SLOT(closeTab(int)));
 
     // apply the saved mainwindow settings, if any, and ask the mainwindow
     // to automatically save settings if changed: window size, toolbar
@@ -339,20 +339,29 @@ void CantorShell::setTabCaption(const QString& caption)
     m_tabWidget->setTabText(m_tabWidget->indexOf(part->widget()), caption);
 }
 
-void CantorShell::closeTab(QWidget* widget)
+void CantorShell::closeTab(int index)
 {
-    if(!reallyClose(false)) {
+    if (!reallyClose(false))
+    {
         return;
     }
-    if(widget==0)
+
+    QWidget* widget = nullptr;
+    if (index >= 0)
     {
-        if(m_part!=0)
-            widget=m_part->widget();
-        else
-            return;
+        widget = m_tabWidget->widget(index);
+    }
+    else if (m_part)
+    {
+        widget = m_part->widget();
     }
 
-    int index=m_tabWidget->indexOf(widget);
+    if (!widget)
+    {
+        qWarning() << "Could not find widget by tab index" << index;
+        return;
+    }
+
 
     m_tabWidget->removeTab(index);
 

src/cantor.h
@@ -83,7 +83,7 @@ private Q_SLOTS:
     void activateWorksheet(int index);
 
     void setTabCaption(const QString& tab);
-    void closeTab(QWidget* widget = 0);
+    void closeTab(int index = -1);
 
     void showSettings();
 