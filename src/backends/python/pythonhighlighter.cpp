22fd4479ab026180703ad171e635df7f0bd316af
Filipe Saraiva <filipe@kde.org>
2018-09-04 06:52:51 -0300

    Remove 'PythonKeywords::instance()->addVariable' because it put the variable to all actives Python sessions

--- src/backends/python/pythonhighlighter.cpp
+++ src/backends/python/pythonhighlighter.cpp
@@ -147,5 +147,4 @@ void PythonHighlighter::updateHighlight()
 void PythonHighlighter::addVariable(const QString variable)
 {
     m_variables << variable;
-    PythonKeywords::instance()->addVariable(variable);
 }
