9ed08bccf5112e68dcfa70c398b8fa0309d4454d
Rex Dieter <rdieter@gmail.com>
2019-07-11 08:44:54 -0500

    support python-3.8

--- cmake/FindPythonLibs3.cmake
+++ cmake/FindPythonLibs3.cmake
@@ -6,10 +6,10 @@ find_path(PYTHONLIBS3_INCLUDE_DIR Python.h
           HINTS ${PYTHONLIBS3_INCLUDEDIR} ${PYTHONLIBS3_INCLUDE_DIRS})
 
 IF(WIN32)
-  find_library(PYTHONLIBS3_LIBRARY NAMES python32 python33 python34 python35 python36 python37
+  find_library(PYTHONLIBS3_LIBRARY NAMES python32 python33 python34 python35 python36 python37 python38
                HINTS ${PYTHONLIBS3_LIBDIR} ${PYTHONLIBS3_LIBRARY_DIRS} )
 ELSE()
-  find_library(PYTHONLIBS3_LIBRARY NAMES python3.2m python3.3m python3.4m python3.5m python3.6m python3.7m
+  find_library(PYTHONLIBS3_LIBRARY NAMES python3.2m python3.3m python3.4m python3.5m python3.6m python3.7m python3.8
                HINTS ${PYTHONLIBS3_LIBDIR} ${PYTHONLIBS3_LIBRARY_DIRS} )
 ENDIF()
 
