d268c4fc7647fbbd71283f24b0b8dac18fc6c8dd
Nikita Sirgienko <warquark@gmail.com>
2019-07-19 20:28:11 +0300

    [Jupyter] Add note about pagecolor in math text template

--- src/mathrendertask.cpp
+++ src/mathrendertask.cpp
@@ -33,6 +33,9 @@
 
 #include "epsrenderer.h"
 
+// Jupyter TODO: pagecolor don't work with preview
+// For example there are question about it:
+// https://tex.stackexchange.com/questions/499712/pagecolor-ignored-when-preview-package-used
 static const QLatin1String mathTex("\\documentclass{minimal}"\
                          "\\usepackage{amsfonts,amssymb}"\
                          "\\usepackage{amsmath}"\
