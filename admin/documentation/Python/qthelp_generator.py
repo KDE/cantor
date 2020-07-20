8479a1fd3573c1473136c4904044dc1ddccb1fbe
Shubham Shubham <aryan100jangid@gmail.com>
2020-07-20 17:02:25 +0530

    Refactor

--- admin/documentation/Python/qthelp_generator.py
+++ admin/documentation/Python/qthelp_generator.py
@@ -39,36 +39,36 @@ qhp.writelines("""<?xml version="1.0" encoding="UTF-8"?>
     <namespace>org.kde.python.3.8.4</namespace>
 
     <virtualFolder>doc</virtualFolder>
-    
+
     <customFilter name="Python">
         <filterAttribute>Python Documentation</filterAttribute>
         <filterAttribute>3.8.4</filterAttribute>
     </customFilter>
-    
+
     <filterSection>
         <toc>
-            <section title="Python 3.8.4 Documentation" ref="python-3.8.4-docs-html/index.html">
-                
-                <section title="Parts of the documentation:" ref="python-3.8.4-docs-html/index.html">
-                    <section title="What's new in Python 3.8?" ref="python-3.8.4-docs-html/whatsnew/3.8.html"></section>
-                    <section title="Tutorial" ref="python-3.8.4-docs-html/tutorial/index.html"></section>
-                    <section title="Library Reference" ref="python-3.8.4-docs-html/library/index.html"></section>
-                    <section title="Language Reference" ref="python-3.8.4-docs-html/reference/index.html"></section>
-                    <section title="Python Setup and Usage" ref="python-3.8.4-docs-html/using/index.html"></section>
-                    <section title="Python HOWTOs" ref="python-3.8.4-docs-html/howto/index.html"></section>
-                    <section title="Installing Python Modules" ref="python-3.8.4-docs-html/installing/index.html"></section>
-                    <section title="Distributing Python Modules" ref="python-3.8.4-docs-html/distributing/index.html"></section>
-                    <section title="Extending and Embedding" ref="python-3.8.4-docs-html/extending/index.html"></section>
-                    <section title="Python/C API" ref="python-3.8.4-docs-html/c-api/index.html"></section>
-                    <section title="FAQs" ref="python-3.8.4-docs-html/faq/index.html"></section>
+            <section title="Python 3.8.4 Documentation" ref="index.html">
+
+                <section title="Parts of the documentation:" ref="index.html">
+                    <section title="What's new in Python 3.8?" ref="whatsnew/3.8.html"></section>
+                    <section title="Tutorial" ref="tutorial/index.html"></section>
+                    <section title="Library Reference" ref="library/index.html"></section>
+                    <section title="Language Reference" ref="reference/index.html"></section>
+                    <section title="Python Setup and Usage" ref="using/index.html"></section>
+                    <section title="Python HOWTOs" ref="howto/index.html"></section>
+                    <section title="Installing Python Modules" ref="installing/index.html"></section>
+                    <section title="Distributing Python Modules" ref="distributing/index.html"></section>
+                    <section title="Extending and Embedding" ref="extending/index.html"></section>
+                    <section title="Python/C API" ref="c-api/index.html"></section>
+                    <section title="FAQs" ref="faq/index.html"></section>
                 </section>
 
-                <section title="Indices and tables:" ref="python-3.8.4-docs-html/index.html">
-                    <section title="Global Module Index" ref="python-3.8.4-docs-html/py-modindex.html"></section>
-                    <section title="General Index" ref="python-3.8.4-docs-html/genindex.html"></section>
-                    <section title="Glossary" ref="python-3.8.4-docs-html/glossary.html"></section>
-                    <section title="Search page" ref="python-3.8.4-docs-html/search.html"></section>
-                    <section title="Complete Table of Contents" ref="python-3.8.4-docs-html/contents.html"></section>
+                <section title="Indices and tables:" ref="index.html">
+                    <section title="Global Module Index" ref="py-modindex.html"></section>
+                    <section title="General Index" ref="genindex.html"></section>
+                    <section title="Glossary" ref="glossary.html"></section>
+                    <section title="Search page" ref="search.html"></section>
+                    <section title="Complete Table of Contents" ref="contents.html"></section>
                 </section>
 
             </section>
@@ -81,23 +81,23 @@ qhp.writelines("""<?xml version="1.0" encoding="UTF-8"?>
 # write the tail
 qhp.writelines("""</keywords>
             <files>
-            <file>python-3.8.4-docs-html/*</file>
-            <file>python-3.8.4-docs-html/_images/*</file>
-            <file>python-3.8.4-docs-html/_sources/*</file>
-            <file>python-3.8.4-docs-html/_static/*</file>
-            <file>python-3.8.4-docs-html/c-api/*</file>
-            <file>python-3.8.4-docs-html/distributing/*</file>
-            <file>python-3.8.4-docs-html/distutils/*</file>
-            <file>python-3.8.4-docs-html/extending/*</file>
-            <file>python-3.8.4-docs-html/faq/*</file>
-            <file>python-3.8.4-docs-html/howto/*</file>
-            <file>python-3.8.4-docs-html/install/*</file>
-            <file>python-3.8.4-docs-html/installing/*</file>
-            <file>python-3.8.4-docs-html/library/*</file>
-            <file>python-3.8.4-docs-html/reference/*</file>
-            <file>python-3.8.4-docs-html/tutorial/*</file>
-            <file>python-3.8.4-docs-html/using/*</file>
-            <file>python-3.8.4-docs-html/whatsnew/*</file>
+            <file>./*</file>
+            <file>_images/*</file>
+            <file>_sources/*</file>
+            <file>_static/*</file>
+            <file>c-api/*</file>
+            <file>distributing/*</file>
+            <file>distutils/*</file>
+            <file>extending/*</file>
+            <file>faq/*</file>
+            <file>howto/*</file>
+            <file>install/*</file>
+            <file>installing/*</file>
+            <file>library/*</file>
+            <file>reference/*</file>
+            <file>tutorial/*</file>
+            <file>using/*</file>
+            <file>whatsnew/*</file>
         </files>
     </filterSection>
 </QtHelpProject> """)
