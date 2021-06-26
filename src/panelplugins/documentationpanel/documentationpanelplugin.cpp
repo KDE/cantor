be1c8e8f0f44ceed7dbd893364de3905803a372b
Alexander Semke <alexander.semke@web.de>
2021-06-26 16:57:09 +0200

    Save and restore the current shown UQL in the web view for the documentation when switching between different worksheet tabs.

--- src/panelplugins/documentationpanel/documentationpanelplugin.cpp
+++ src/panelplugins/documentationpanel/documentationpanelplugin.cpp
@@ -1,27 +1,11 @@
 /*
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA  02110-1301, USA.
-
-    ---
-    Copyright (C) 2020 Shubham <aryan100jangid@gmail.com>
-    Copyright (C) 2020 Alexander Semke <alexander.semke@web.de>
- */
-
-#include "cassert"
+    SPDX-License-Identifier: GPL-2.0-or-later
+    SPDX-FileCopyrightText: 2020 Shubham <aryan100jangid@gmail.com>
+    SPDX-FileCopyrightText: 2020-2021 Alexander Semke <alexander.semke@web.de>
+*/
 
 #include "documentationpanelplugin.h"
+#include "documentationpanelwidget.h"
 #include "session.h"
 
 DocumentationPanelPlugin::DocumentationPanelPlugin(QObject* parent, QList<QVariant> args) : Cantor::PanelPlugin(parent)
@@ -58,6 +42,7 @@ void DocumentationPanelPlugin::connectToShell(QObject* cantorShell)
 Cantor::PanelPlugin::State DocumentationPanelPlugin::saveState()
 {
     Cantor::PanelPlugin::State state = PanelPlugin::saveState();
+    state.inners.append(m_widget->url()); //save the currently shown URL in the web view
     return state;
 }
 
@@ -65,7 +50,11 @@ void DocumentationPanelPlugin::restoreState(const Cantor::PanelPlugin::State& st
 {
     PanelPlugin::restoreState(state);
     if(session() && m_widget)
+    {
         m_widget->updateBackend(session()->backend()->name());
+        if (state.inners.size() == 1)
+            m_widget->showUrl(state.inners.first().toUrl());
+    }
 }
 
 K_PLUGIN_FACTORY_WITH_JSON(documentationpanelplugin, "documentationpanelplugin.json", registerPlugin<DocumentationPanelPlugin>();)
--- src/panelplugins/documentationpanel/documentationpanelwidget.h
+++ src/panelplugins/documentationpanel/documentationpanelwidget.h
@@ -1,23 +1,8 @@
 /*
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA  02110-1301, USA.
-
-    ---
-    Copyright (C) 2020 Shubham <aryan100jangid@gmail.com>
-    Copyright (C) 2020 Alexander Semke <alexander.semke@web.de>
- */
+    SPDX-License-Identifier: GPL-2.0-or-later
+    SPDX-FileCopyrightText: 2020 Shubham <aryan100jangid@gmail.com>
+    SPDX-FileCopyrightText: 2020-2021 Alexander Semke <alexander.semke@web.de>
+*/
 
 #ifndef _DOCUMENTATIONPANELWIDGET_H
 #define _DOCUMENTATIONPANELWIDGET_H
@@ -47,7 +32,7 @@ class DocumentationPanelWidget : public QWidget
     ~DocumentationPanelWidget();
 
     void updateBackend(const QString&);
-    const QUrl& url() const;
+    QUrl url() const;
 
   public Q_SLOTS:
     void showUrl(const QUrl&);
