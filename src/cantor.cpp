KDE/cantor

commit fd4e9e0e4b9e1c2d346fdc787f76f63a8a795aeb

Show the default help information in the help panel only after the backend was already completely initialized. This avoids the crash when openning project files without having any worksheet tab created yet.
BUG: 459631
FIXED-IN: 22.08.2

src/cantor.cpp
@@ -360,8 +360,12 @@ void CantorShell::addWorksheet(const QString& backendName)
             connect(part, SIGNAL(requestDocumentation(QString)), this, SIGNAL(requestDocumentation(QString)));
 
             m_parts.append(part);
-            if (backend) // If backend empty (loading worksheet from file), then we connect to signal and wait
+            if (backend) {// If backend empty (loading worksheet from file), then we connect to signal and wait
                 m_parts2Backends[part] = backend->id();
+
+                //show the default help string in the help panel
+                emit showHelp(backend->defaultHelp());
+            }
             else
             {
                 m_parts2Backends[part] = QString();
@@ -375,9 +379,6 @@ void CantorShell::addWorksheet(const QString& backendName)
 
             // Force run updateCaption for getting proper backend icon
             QMetaObject::invokeMethod(part, "updateCaption");
-
-            //show the default help string in the help panel
-            emit showHelp(backend->defaultHelp());
         }
         else
         {
@@ -881,20 +882,27 @@ void CantorShell::saveDockPanelsState(KParts::ReadWritePart* part)
     }
 }
 
-void CantorShell::updateBackendForPart(const QString& backend)
+void CantorShell::updateBackendForPart(const QString& backendName)
 {
     auto* part = dynamic_cast<KParts::ReadWritePart*>(sender());
     if (part && m_parts2Backends.contains(part) && m_parts2Backends[part].isEmpty())
     {
-        m_parts2Backends[part] = backend;
+        m_parts2Backends[part] = backendName;
 
         KConfigGroup panelStatusGroup(KSharedConfig::openConfig(), QLatin1String("PanelsStatus"));
-        if (m_part == part && panelStatusGroup.hasKey(backend))
+        if (m_part == part && panelStatusGroup.hasKey(backendName))
         {
             const QStringList& plugins = panelStatusGroup.readEntry(m_parts2Backends[m_part]).split(QLatin1Char('\n'));
             m_pluginsVisibility[m_part] = plugins;
 
             updatePanel();
+        }
+
+        auto* backend = Cantor::Backend::getBackend(backendName);
+        if (backend)
+        {
+            //show the default help string in the help panel
+            emit showHelp(backend->defaultHelp());
         }
     }
 }

commit 28080396635e03863cac294482b1e0bcb819ebc0

Show the default help string in the help panel _after_ the panels were initialized. This solves the problem with nothing shown in the help panel after a new worksheet was created.
Regression introduced in fd4e9e0e& when fixing another issue.

src/cantor.cpp
@@ -1,7 +1,7 @@
 /*
     SPDX-License-Identifier: GPL-2.0-or-later
     SPDX-FileCopyrightText: 2009 Alexander Rieder <alexanderrieder@gmail.com>
-    SPDX-FileCopyrightText: 2018-2022 Alexander Semke <alexander.semke@web.de>
+    SPDX-FileCopyrightText: 2018-2023 Alexander Semke <alexander.semke@web.de>
 */
 #include "cantor.h"
 #include "lib/session.h"
@@ -30,7 +30,6 @@
 #include <QGraphicsView>
 #include <QPushButton>
 #include <QRegularExpression>
-
 
 #include "lib/backend.h"
 #include "lib/worksheetaccess.h"
@@ -360,16 +359,15 @@ void CantorShell::addWorksheet(const QString& backendName)
         connect(part, SIGNAL(requestDocumentation(QString)), this, SIGNAL(requestDocumentation(QString)));
 
         m_parts.append(part);
-        if (backend) {// If backend empty (loading worksheet from file), then we connect to signal and wait
+        if (backend)
             m_parts2Backends[part] = backend->id();
-
-            //show the default help string in the help panel
-            emit showHelp(backend->defaultHelp());
-        } else
+        else
         {
+            // if the backend is empty (loading worksheet from file), connect to the signal and wait
             m_parts2Backends[part] = QString();
             connect(part, SIGNAL(setBackendName(QString)), this, SLOT(updateBackendForPart(QString)));
         }
+
         int tab = m_tabWidget->addTab(part->widget(), i18n("Session %1", sessionCount++));
         m_tabWidget->setCurrentIndex(tab);
         // Setting focus on worksheet view, because Qt clear focus of added widget inside addTab
@@ -378,6 +376,10 @@ void CantorShell::addWorksheet(const QString& backendName)
 
         // Force run updateCaption for getting proper backend icon
         QMetaObject::invokeMethod(part, "updateCaption");
+
+        //all panels initialized, show the default help string in the help panel
+        if (backend)
+            emit showHelp(backend->defaultHelp());
     }
     else
     {