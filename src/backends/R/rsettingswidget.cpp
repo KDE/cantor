6de248339e73d9bede870dbd0ad6dc865456f8a8
Alexander Semke <alexander.semke@web.de>
2025-03-09 14:04:24 +0100

    Added the missing hanlding of "integrated plots" settings for R, Python and Julia.

--- src/backends/R/rsettingswidget.cpp
+++ src/backends/R/rsettingswidget.cpp
@@ -1,7 +1,7 @@
 /*
     SPDX-License-Identifier: GPL-2.0-or-later
     SPDX-FileCopyrightText: 2010 Oleksiy Protas <elfy.ua@gmail.com>
-    SPDX-FileCopyrightText: 2020 Alexander Semke <alexander.semke@web.de>
+    SPDX-FileCopyrightText: 2020-2025 Alexander Semke <alexander.semke@web.de>
 */
 
 #include "rsettingswidget.h"
@@ -9,6 +9,7 @@
 #include <QFileDialog>
 #include <QLineEdit>
 #include <QMouseEvent>
+#include <QTimer>
 #include <KLocalizedString>
 
 RSettingsWidget::RSettingsWidget(QWidget *parent, const QString& id) : BackendSettingsWidget(parent, id)
@@ -22,10 +23,30 @@ RSettingsWidget::RSettingsWidget(QWidget *parent, const QString& id) : BackendSe
     tabWidget->removeTab(2);
 #endif
     connect(tabWidget, &QTabWidget::currentChanged, this, &BackendSettingsWidget::tabChanged);
+    connect(kcfg_integratePlots, &QCheckBox::clicked, this, &RSettingsWidget::integratePlotsChanged);
 
     kcfg_autorunScripts->lineEdit()->setReadOnly(true);
     kcfg_autorunScripts->lineEdit()->installEventFilter(this);
     kcfg_autorunScripts->lineEdit()->setToolTip(i18n("Double click to open file selection dialog"));
+
+    kcfg_inlinePlotFormat->setItemIcon(0, QIcon::fromTheme(QLatin1String("application-pdf")));
+    kcfg_inlinePlotFormat->setItemIcon(1, QIcon::fromTheme(QLatin1String("image-svg+xml")));
+    kcfg_inlinePlotFormat->setItemIcon(2, QIcon::fromTheme(QLatin1String("image-png")));
+
+    // return to the event loop to show the settings
+    // and then call the slot to update the state of the widgets
+    QTimer::singleShot(0, this, [=]() {
+        integratePlotsChanged(kcfg_integratePlots->isChecked());
+    });
+}
+
+void RSettingsWidget::integratePlotsChanged(bool state)
+{
+    lPlotWidth->setEnabled(state);
+    kcfg_plotWidth->setEnabled(state);
+    lPlotHeight->setEnabled(state);
+    kcfg_plotHeight->setEnabled(state);
+    kcfg_inlinePlotFormat->setEnabled(state);
 }
 
 bool RSettingsWidget::eventFilter(QObject *obj, QEvent *event)
--- src/backends/julia/juliasettingswidget.cpp
+++ src/backends/julia/juliasettingswidget.cpp
@@ -1,24 +1,10 @@
 /*
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA  02110-1301, USA.
-
-    ---
-    Copyright (C) 2020-2023 Alexander Semke <alexander.semke@web.de>
- */
+    SPDX-FileCopyrightText: 2020-2025 by Alexander Semke (alexander.semke@web.de)
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
 
 #include "juliasettingswidget.h"
+#include <QTimer>
 
 JuliaSettingsWidget::JuliaSettingsWidget(QWidget* parent, const QString& id) : BackendSettingsWidget(parent, id)
 {
@@ -32,4 +18,25 @@ JuliaSettingsWidget::JuliaSettingsWidget(QWidget* parent, const QString& id) : B
 #endif
 
     connect(tabWidget, &QTabWidget::currentChanged, this, &BackendSettingsWidget::tabChanged);
+    connect(kcfg_integratePlots, &QCheckBox::clicked, this, &JuliaSettingsWidget::integratePlotsChanged);
+
+    kcfg_inlinePlotFormat->setItemIcon(0, QIcon::fromTheme(QLatin1String("application-pdf")));
+    kcfg_inlinePlotFormat->setItemIcon(1, QIcon::fromTheme(QLatin1String("image-svg+xml")));
+    kcfg_inlinePlotFormat->setItemIcon(2, QIcon::fromTheme(QLatin1String("image-png")));
+
+    // return to the event loop to show the settings
+    // and then call the slot to update the state of the widgets
+    QTimer::singleShot(0, this, [=]() {
+        integratePlotsChanged(kcfg_integratePlots->isChecked());
+    });
+}
+
+void JuliaSettingsWidget::integratePlotsChanged(bool state)
+{
+    lPlotWidth->setEnabled(state);
+    kcfg_plotWidth->setEnabled(state);
+    lPlotHeight->setEnabled(state);
+    kcfg_plotHeight->setEnabled(state);
+    kcfg_inlinePlotFormat->setEnabled(state);
+    kcfg_plotExtenstionGraphicPackage->setEnabled(state);
 }
